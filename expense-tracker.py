62d1e9d7a15b32001c4799a82092e354f3b1e26d
Filza Syed <aroobasyed210@gmail.com>
2026-07-19 21:18:33 +0500

    Create repeating command line menu

--- expense-tracker.py
+++ expense-tracker.py
@@ -1,1 +1,33 @@
-print("Expense Tracker")
+def display_menu():
+    print("===== Expense Tracker =====")
+    print("1. Add Expense")
+    print("2. View Expenses")
+    print("3. View Total")
+    print("4. Search by Category")
+    print("5. Exit")
+
+running = True
+while running:
+    
+    display_menu()
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        print("Add Expense - Coming Soon!\n")
+
+    elif choice == "2":
+        print("View Expenses - Coming Soon!\n")
+
+    elif choice == "3":
+        print("View Total - Coming Soon!\n")
+
+    elif choice == "4":
+        print("Search Category - Coming Soon!\n")
+
+    elif choice == "5":
+        print("Goodbye!\n")
+        running = False
+
+    else:
+        print("Invalid option.\n")
+
