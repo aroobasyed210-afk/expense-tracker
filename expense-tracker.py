f7b77cba7f0b217c6eb5fb5e2e49882eb8b135ef
Filza Syed <aroobasyed210@gmail.com>
2026-07-21 13:12:28 +0500

    Add file persistence for expenses

--- expense-tracker.py
+++ expense-tracker.py
@@ -1,3 +1,5 @@
+import json 
+
 expenses = []
 def add_expense():
     amount = input("Enter amount: ")
@@ -11,7 +13,24 @@ def add_expense():
     }
 
     expenses.append(expense)
+    save_expenses()
     print("Expense added successfully!\n")
+
+def save_expenses():
+    with open("expenses.json", "w") as file:
+        json.dump(expenses, file, indent=4)
+
+def load_expenses():
+    global expenses
+
+    try:
+        with open("expenses.json", "r") as file:
+            expenses = json.load(file)
+                    
+    except FileNotFoundError:       #try block might fail with first run
+        expenses = []               # if expenses.json doesn't exist
+
+load_expenses()
 
 def display_menu():
     print("===== Expense Tracker =====")
@@ -22,8 +41,8 @@ def display_menu():
     print("5. Exit")
 
 running = True
+
 while running:
-    
     display_menu()
     choice = input("Choose an option: ")
 
